79cfcf3bc2b94f050f513e36009b0939198f1e94
Tobias <tholetzko@users.noreply.github.com>
2017-04-20 00:45:25 -0500

    Homework completed

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -10,14 +10,21 @@ class CalculationsController < ApplicationController
     # The special word the user input is in the string @special_word.
     # ================================================================================
 
+    # Word count
+    text_split_into_array = @text.gsub(/[^a-z0-9\s]/i, "").downcase.split
+    @word_count = text_split_into_array.length
 
-    @word_count = "Replace this string with your answer."
+    # Character count
+    characters_split_into_array = @text.chars
+    number_of_spaces = @text.count(' ')
 
-    @character_count_with_spaces = "Replace this string with your answer."
+    @character_count_with_spaces = characters_split_into_array.length
+    @character_count_without_spaces = characters_split_into_array.length - number_of_spaces
 
-    @character_count_without_spaces = "Replace this string with your answer."
+    # Occurence count
+    special_word_downcase = @special_word.downcase
 
-    @occurrences = "Replace this string with your answer."
+    @occurrences = text_split_into_array.count(special_word_downcase)
 
     # ================================================================================
     # Your code goes above.
@@ -38,7 +45,10 @@ class CalculationsController < ApplicationController
     # The principal value the user input is in the decimal @principal.
     # ================================================================================
 
-    @monthly_payment = "Replace this string with your answer."
+    r = @apr.to_f / 100 / 12
+    p = @principal.to_f
+    n = @years * 12
+    @monthly_payment = ( r / ( 1 - (1 + r) ** (-1 * n))) * p
 
     # ================================================================================
     # Your code goes above.
@@ -60,12 +70,12 @@ class CalculationsController < ApplicationController
     #   number of seconds as a result.
     # ================================================================================
 
-    @seconds = "Replace this string with your answer."
-    @minutes = "Replace this string with your answer."
-    @hours = "Replace this string with your answer."
-    @days = "Replace this string with your answer."
-    @weeks = "Replace this string with your answer."
-    @years = "Replace this string with your answer."
+    @seconds = @ending - @starting
+    @minutes = @seconds / 60
+    @hours = @minutes / 60
+    @days = @hours / 24
+    @weeks = @days / 7
+    @years = @weeks / 52
 
     # ================================================================================
     # Your code goes above.
@@ -82,27 +92,35 @@ class CalculationsController < ApplicationController
     # The numbers the user input are in the array @numbers.
     # ================================================================================
 
-    @sorted_numbers = "Replace this string with your answer."
+    @sorted_numbers = @numbers.sort_by(&:to_i)
 
-    @count = "Replace this string with your answer."
+    @count = @numbers.count
 
-    @minimum = "Replace this string with your answer."
+    @minimum = @numbers.min
 
-    @maximum = "Replace this string with your answer."
+    @maximum = @numbers.max
 
-    @range = "Replace this string with your answer."
+    @range = @maximum - @minimum
+    len = @numbers.length
+    sorted = @numbers.sort
+    @median = len % 2 == 1 ? sorted[len/2] : (sorted[len/2 - 1] + sorted[len/2]).to_f / 2
 
-    @median = "Replace this string with your answer."
+    @sum = @numbers.sum
 
-    @sum = "Replace this string with your answer."
+    @mean = @sum / @count
 
-    @mean = "Replace this string with your answer."
+    squared_numbers = []
 
-    @variance = "Replace this string with your answer."
+    @numbers.each do |num|
+      square = (num - @mean) ** 2
+      squared_numbers.push(square)
+    end
 
-    @standard_deviation = "Replace this string with your answer."
+    @variance = squared_numbers.sum / @count
 
-    @mode = "Replace this string with your answer."
+    @standard_deviation = Math.sqrt(@variance)
+
+    @mode = @numbers.max_by { |i| @numbers.count(i) }
 
     # ================================================================================
     # Your code goes above.
